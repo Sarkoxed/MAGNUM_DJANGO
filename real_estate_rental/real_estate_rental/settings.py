3fec2d3c1fbf626523e74958312df3af8db4e25b
Sarkoxed <ss2003@yandex.ru>
2023-12-29 09:48:34 +0300

    chekc

--- real_estate_rental/real_estate_rental/settings.py
+++ real_estate_rental/real_estate_rental/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIR = [
-        os.path.join(BASE_DIR, "rental_app/static")
+        os.path.join(BASE_DIR, "static")
         ]
 
 # Default primary key field type
